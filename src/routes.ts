4f0a6da747953f29ad53ebb6b76f69f64ca9dc95
peterhttps <plrf100@gmail.com>
2021-10-14 23:00:55 -0300

    feature(middleware): implementing authentication middlewaare

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 import { AuthenticateUserController } from './useCases/authenticateUser/AuthenticateUserController';
 import { CreateUserController } from './useCases/createUser/CreateUserController';
 
@@ -10,5 +11,15 @@ const authenticateUserController = new AuthenticateUserController();
 
 router.post('/users', createUserController.handle);
 router.post('/login', authenticateUserController.handle);
+
+router.get('/courses', ensureAuthenticated, (request, response) => {
+  return response.json([
+    { id: 1, name: "ReactJS" },
+    { id: 2, name: "AngularJS" },
+    { id: 3, name: "VueJS" },
+    { id: 4, name: "React Native" },
+    { id: 5, name: "Flutter" },
+  ])
+});
 
 export { router };
